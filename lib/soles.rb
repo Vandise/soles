22cd1a5fd992be7c6fada5ec8b66ee23fd031b91
Chris Heald <cheald@gmail.com>
2017-07-11 03:19:32 -0700

    Don't need our own mattr_accessor

--- lib/soles.rb
+++ lib/soles.rb
@@ -1,23 +1,11 @@
 require "soles/version"
+require "active_support/core_ext/module/attribute_accessors"
 
 module Soles
   autoload :Application, "soles/application"
   autoload :Configuration, "soles/configuration"
   autoload :Generator, "soles/generator"
   autoload :Controller, "soles/controller"
-
-  def self.mattr_accessor(*names)
-    names.each do |name|
-      define_method("#{name}") do
-        instance_variable_get("@#{name}")
-      end
-
-      define_method("#{name}=") do |arg|
-        instance_variable_set("@#{name}", arg)
-      end
-      module_function :"#{name}=", :"#{name}"
-    end
-  end
 
   mattr_accessor :environment, :configuration, :root
 end
